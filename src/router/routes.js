e90daff9bc25b5b3fa05e60c76cdb204bdbc51d4
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-03-03 23:04:18 +0700

    sgs

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,25 @@
 
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/ExecutiveLayout.vue'),
+  //   children: [
+  //     // { path: '', component: () => import('pages/direktur/IndexPage.vue') },
+  //     { path: '', redirect: '/keuangan' },
+  //     { path: '/splash', name: 'splash', component: () => import('pages/direktur/splash/SplashPage.vue') },
+  //     { path: '/keuangan', name: 'keuangan', component: () => import('pages/direktur/keuangan/KeuanganPage.vue') },
+  //     { path: '/kepegawaian', name: 'kepegawaian', component: () => import('pages/direktur/kepegawaian/KepegawaianPage.vue') },
+  //     { path: '/layanan', name: 'layanan', component: () => import('pages/direktur/pelayanan/PelayananPage.vue') },
+  //     { path: '/sarpras', name: 'sarpras', component: () => import('pages/direktur/sarpras/SarprasPage.vue') }
+  //   ]
+  // },
   {
     path: '/',
-    component: () => import('layouts/ExecutiveLayout.vue'),
-    children: [
-      // { path: '', component: () => import('pages/direktur/IndexPage.vue') },
-      { path: '', redirect: '/keuangan' },
-      { path: '/splash', name: 'splash', component: () => import('pages/direktur/splash/SplashPage.vue') },
-      { path: '/keuangan', name: 'keuangan', component: () => import('pages/direktur/keuangan/KeuanganPage.vue') },
-      { path: '/kepegawaian', name: 'kepegawaian', component: () => import('pages/direktur/kepegawaian/KepegawaianPage.vue') },
-      { path: '/layanan', name: 'layanan', component: () => import('pages/direktur/pelayanan/PelayananPage.vue') },
-      { path: '/sarpras', name: 'sarpras', component: () => import('pages/direktur/sarpras/SarprasPage.vue') }
-    ]
-  },
-  {
-    path: '/anjungan',
     component: () => import('layouts/AnjunganLayout.vue'),
     children: [
       // { path: '', component: () => import('pages/direktur/IndexPage.vue') },
-      { path: '', redirect: '/anjungan/rsud' },
-      { path: '/anjungan/rsud', name: 'anjungan.rsud', component: () => import('pages/anjungan/IndexPage.vue') }
+      { path: '', redirect: '/anjungan' },
+      { path: '/anjungan', name: 'anjungan', component: () => import('pages/anjungan/IndexPage.vue') }
     ]
   },
 
