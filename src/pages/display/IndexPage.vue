ac063045e1966b661528b4486144e6d4ae94460d
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-03-29 22:11:51 +0700

    yyt

--- src/pages/display/IndexPage.vue
+++ src/pages/display/IndexPage.vue
@@ -28,8 +28,24 @@
       <div class="col grow bg-negative">
         <div class="row full-height">
           <div class="col-4 bg-secondary">sfa</div>
-          <div class="col-8 bg-grey">sfa</div>
+          <div class="col-8 bg-grey">
+            <div class="column full-height">
+              <div class="col tempat-video">
+                <div class="q-pa-xs">
+                  <q-video
+                    ratio="1.3"
+                    src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+                  />
+                </div>
+              </div>
+              <div class="col-2 tempat-list-antrian bg-teal">Tempat List</div>
+            </div>
+          </div>
         </div>
+      </div>
+      <!-- text berjalan -->
+      <div class="col-auto">
+        berjalan
       </div>
     </div>
 
