736bd50ab0331c9a6e7e86639cfed020f02e3342
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-03-30 10:47:23 +0700

    sss

--- quasar.config.js
+++ quasar.config.js
@@ -29,7 +29,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
 
-      'axios', 'globalComponents', 'plugins'
+      'axios', 'globalComponents'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
