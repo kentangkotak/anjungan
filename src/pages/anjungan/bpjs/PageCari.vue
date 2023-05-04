9b0d9c26972a636c27ba1a7f7c5c93b19c3a9d70
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-05-04 10:31:42 +0700

    saved2

--- src/pages/anjungan/bpjs/PageCari.vue
+++ src/pages/anjungan/bpjs/PageCari.vue
@@ -1,29 +1,43 @@
 <template>
-  <div class="q-pt-lg q-mt-lg">
-    <div class="text-h3 text-weight-bold">Pasien BPJS</div>
-    <q-separator class="q-my-lg q-mr-lg"></q-separator>
-    <div class="info text-body1" style="margin-top:100px;">
-      Silahkan Anda cari berdasarkan <b>No RUJUKAN </b> yang didapat dari FASKES Tingkat I / Rumah Sakit Lain ,
-      <span>Atau <b>SURAT KONTROL </b> Dari RSUD MOH SALEH dibawah Berikut: </span>
+  <div class="q-pa-xs full-height bg-white" style="border-radius: 5px;">
+    <!-- <q-card flat rounded bordered class="bg-grey-2"> -->
+    <div class="q-pa-md q-my-md">
+      <div class="text-h3 text-weight-bold">Pasien BPJS</div>
+      <q-separator class="q-my-lg"></q-separator>
+      <div class="info text-body1" style="margin-top:100px;">
+        Silahkan Anda cari berdasarkan <b>No RUJUKAN </b> yang didapat dari FASKES Tingkat I / Rumah Sakit Lain ,
+        <span>Atau <b>SURAT KONTROL </b> Dari RSUD MOH SALEH dibawah Berikut: </span>
+      </div>
+      <div class="q-my-lg">
+        <app-input-kiosk
+          ref="kiosk"
+          v-model="search"
+          icon="search"
+          :capslock="caps"
+          @key-clicked="(val)=> keyClicked(val)"
+          @clearable="search=''"
+          label="Cari no Rujukan / Surat Kontrol"
+        />
+      </div>
+      <!-- <div class="absolute-bottom">
+        <q-btn
+        icon="keyboard_return"
+        color="negative" size="lg" padding="md" label="Kembali"
+        to="/"
+        />
+      </div> -->
+      <div class="absolute-bottom q-pa-md">
+      <div class="row full-width">
+        <div class="col-grow bg-negative cursor-pointer" @click="goTo('/anjungan')">
+          <div class="q-pa-lg text-center text-white f-20">KEMBALI</div>
+        </div>
+        <!-- <div class="col-grow bg-dark cursor-pointer">
+          <div class="q-pa-lg text-center text-white">SETUJU</div>
+        </div> -->
+      </div>
     </div>
-    <div class="q-my-lg q-mr-lg">
-      <app-input-kiosk
-        ref="kiosk"
-        v-model="search"
-        icon="search"
-        :capslock="caps"
-        @key-clicked="(val)=> keyClicked(val)"
-        @clearable="search=''"
-        label="Cari no Rujukan / Surat Kontrol"
-      />
     </div>
-    <div class="absolute-bottom text-right q-pa-lg">
-      <q-btn
-      icon="keyboard_return"
-      color="negative" size="lg" padding="md" label="Kembali"
-      to="/"
-      />
-    </div>
+  <!-- </q-card> -->
   </div>
 </template>
 
@@ -31,12 +45,13 @@
 import 'vue3-lottie/dist/style.css'
 import { ref } from 'vue'
 import { useBpjsStore } from 'src/stores/anjungan/bpjs'
+import { useRouter } from 'vue-router'
 
 const search = ref('')
 const kiosk = ref(null)
 const caps = ref(true)
 
-// const tab = ref('awal')
+const router = useRouter()
 const store = useBpjsStore()
 
 function keyClicked (e) {
@@ -54,5 +69,10 @@ function keyClicked (e) {
   } else {
     search.value += e
   }
+}
+
+function goTo (val) {
+  store.changeClasses()
+  router.push(val)
 }
 </script>
---
+++ src/pages/anjungan/bpjs/PageResult.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="q-pa-md full-height bg-white" style="border-radius: 5px;">
+    <q-card flat rounded bordered class="bg-grey-2">
+      <q-card-section>
+        <div>Data Pasien</div>
+        <div class="row q-mt-lg">
+          <div>
+            <q-avatar
+              size="100px"
+            >
+              <img :src="`https://cdn.quasar.dev/img/avatar1.jpg`" style="border: 2px solid blue;">
+            </q-avatar>
+          </div>
+          <div class="q-ml-lg">
+            <div class="flex">
+              <div class="w-t">Nama</div>
+              <div class="text-weight-bold" >: Nama Pasien </div>
+            </div>
+            <div class="flex">
+              <div class="w-t">No. RM</div>
+              <div class="text-weight-bold" >: No RM Pasien </div>
+            </div>
+            <div class="flex">
+              <div class="w-t">Alamat</div>
+              <div class="text-weight-bold" >: Alamat Pasien Alamat Pasien</div>
+            </div>
+
+            <q-separator class="q-my-lg"></q-separator>
+            <div>
+              Anda Akan ke poli GIGI
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+
+    </q-card>
+    <!-- <div class="q-mt-lg">Apakah Benar Informasi di atas?</div> -->
+    <div class="q-mt-lg">Apakah Informasi diatas sudah benar? <br />
+      Jika sudah benar ... tekan <span class="text-weight-bold"> SETUJU </span> untuk
+      <span class="text-weight-bold"> PRINT ANTREAN POLI </span> Anda <br/><br/>
+      Atau tekan <span class="text-weight-bold"> KEMBALI </span> Jika Anda ingin melakukan pencarian ulang Data Anda
+    </div>
+
+    <div class="absolute-bottom q-pa-md">
+      <div class="row full-width">
+        <div class="col-grow bg-negative cursor-pointer" @click="store.tab='awal'">
+          <div class="q-pa-lg text-center text-white f-20">KEMBALI</div>
+        </div>
+        <div class="col-grow bg-dark cursor-pointer">
+          <div class="q-pa-lg text-center text-white f-20">SETUJU</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useBpjsStore } from 'src/stores/anjungan/bpjs'
+
+const store = useBpjsStore()
+</script>
